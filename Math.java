0e286f8cbcd18005dcc5918e5984296d438275fc
Juan <mrjuancod94@gmail.com>
2017-05-10 11:21:27 +0200

    Se añaden ficheros y librerias al proyecto

---
+++ Math.java
@@ -0,0 +1,13 @@
+public class Math {
+	int a,b;
+	Math(int a,int b) {
+		this.a=a;
+		this.b=b;
+	}
+	public int add() {
+		return a+b;
+	}
+	public static void main (String[] args){
+		Math m= new Math(2,2);	
+	}
+}
